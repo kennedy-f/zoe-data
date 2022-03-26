e26bb9833a0d01cf3a14a0be6982a2549c0a81ad
Kennedy Silva <kennedy2000silva@hotmail.com>
2022-03-26 16:10:36 -0300

    fix: app.tsx wrong import

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { ApiConfigProvider } from "./context";
+import { ApiConfigProvider } from "context/ApiConfig/ApiConfig.provider";
 
 export function App() {
   return (
     <ApiConfigProvider
       axiosConfigParams={{ baseURL: "https://api.github.com/" }}
-    ></ApiConfigProvider>
+    >
+      <> </>
+    </ApiConfigProvider>
   );
 }
