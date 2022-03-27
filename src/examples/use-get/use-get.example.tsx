ca58b930242a9d7c4245deaf4097673af3a7e7f3
Kennedy Silva <kennedy2000silva@hotmail.com>
2022-03-27 20:21:13 -0300

    useGet example

--- src/examples/use-get/use-get.example.tsx
+++ src/examples/use-get/use-get.example.tsx
@@ -1,3 +1,31 @@
 import React from "react";
+import { useGet } from "../../main";
 
-export function UseGetExample() {}
+interface ResponseData {
+  id: number;
+  name: string;
+}
+
+export function UseGetExample() {
+  const { data, loading, refetch, error } =
+    useGet<ResponseData[]>("/api/users");
+
+  if (error) {
+    return <h3> Return your error boundary </h3>;
+  }
+
+  return (
+    <>
+      {loading && <h3> Loading... </h3>}
+      <div>
+        <button onClick={() => refetch()}> load again</button>
+        {data?.map(({ id, name }, index) => (
+          <div key={index}>
+            <p>{id}</p>
+            <h2>{name}</h2>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
